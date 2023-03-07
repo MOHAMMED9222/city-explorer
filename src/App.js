a99258a79cb513be270b8449324d4f0be86edf87
Mohammed Abubakar <mohammedfahim363@yahoo.com>
2023-03-07 09:24:10 -0800

    keyerror

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+import Form from './Form';
+import List from './List';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cityexplorerData: [],
+      cityName: '',
+      cityData: {},
+      latitude: '',
+      longitude: '',
+    }
+  }
+
+
+  handleCityInput = (e) => {
+    this.setState({
+      cityName: e.target.value
+    });
+  };
+
+  eventHandler = async (e) => {
+    e.preventDefault();
+
+    // console.log('event fired');
+    // get the data from the SW API
+    // axios is the library of code that we will use to make our requests
+    let cityexplorercharacters = await axios.get(`https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.cityName}&format=json`);
+    // console.log(cityexplorercharacters.data.results);
+    //save it in state
+    this.setState({
+      cityData: cityexplorercharacters.data[0],
+      latitude: cityexplorercharacters.data[0].latitude,
+      longitude: cityexplorercharacters.data[0].longitude
+    });
+
+  };
+
+
+
+  render() {
+    let mapURL = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=47.6038321,-122.330062&zoom=12`;
+    return (
+      <>
+        <main>
+          <Form
+            handleCityInput={this.handleCityInput}
+            eventHandler={this.eventHandler}
+            cityData={this.state.cityData}
+          />
+          <List
+            data={this.state.cityData}
+          />
+
+        </main>
+
+
+      </>
+    );
+  }
 }
+
+
 
 export default App;
